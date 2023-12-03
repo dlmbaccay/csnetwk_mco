a95c07f2083c58ac3bcc24488ceeb3aae5ded168
Dominic Baccay <dlmbaccay@gmail.com>
2023-12-03 17:53:39 +0800

    broken server-client connection

---
+++ client.py
@@ -0,0 +1,216 @@
+import socket, os, selectors, tkinter as tk
+from tkinter import scrolledtext
+
+# create a socket object
+clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+selector = selectors.DefaultSelector()
+
+hasJoined = False
+
+def register_command():
+    global hasJoined, clientsocket
+
+    command = input_field.get()
+
+    if not len(command):
+        return # do nothing if command is empty
+    
+    parameters = command.split()
+    prefix_command = parameters[0]
+
+    if hasJoined == False:
+        # allow /join, /leave, /? initially
+        if (prefix_command not in ['/join', '/leave', '/?']):
+            # print("Error: Please join the server first.")
+            register_status(system_output, "Error: Please join the server first.")
+            input_field.delete(0, tk.END)
+            return
+        
+        if prefix_command == '/join':
+            if len(parameters) != 3:
+                # print("Error: Command parameters mismatch.")
+                register_status(system_output, "Error: Command parameters mismatch.")
+                return
+
+            try:
+                # connect to server
+                clientsocket.connect((parameters[1], int(parameters[2])))
+                # selector.register(clientsocket, selectors.EVENT_READ) # for reading data from server
+                # print("Successfully joined the server.")
+                register_status(system_output, "Successfully joined the server.")
+                hasJoined = True
+            except socket.error:
+                # print("Error: ", e)
+                register_status(system_output, f"Error: Connection interrupted.")
+                return
+            
+        if prefix_command == '/leave':
+            # print("Error: You have not joined the server yet.")
+            register_status(system_output, "Error: You have not joined the server yet.")
+            return
+        
+        if prefix_command == '/?':
+            message = """
+            /? - list all commands
+            /join <server_ip> <port> - join the server
+            /leave - leave the server
+            """
+
+            # print(message)
+            register_status(system_output, message)
+            return
+    
+    elif hasJoined == True:
+        # allowed commands: /join, /register, /leave, /?, /dir, /store, /get, /message, /broadcast
+        if prefix_command not in ['/join', '/register', '/leave', '/?', '/dir', '/store', '/get', '/message', '/broadcast']:
+            # print("Error: Invalid command.")
+            register_status(system_output, "Error: Invalid command.")
+            return
+        
+        if prefix_command == '/join':
+            if len(parameters) != 3:
+                # print("Error: Command parameters mismatch.")
+                register_status(system_output, "Error: Command parameters mismatch.")
+                return
+            else:
+                # print("Error: You have already joined the server.")
+                register_status(system_output, "Error: You have already joined the server.")
+                return
+        
+        if prefix_command == '/store':
+            if len(parameters) != 2:
+                # print("Error: Command parameters mismatch.")
+                register_status(system_output, "Error: Command parameters mismatch.")
+                return
+            
+            files = os.listdir('client_dir')
+            if parameters[1] not in files:
+                # print("Error: File does not exist.")
+                register_status(system_output, "Error: File does not exist.")
+                return
+            
+            # send file to server
+            with open(f'client_dir/{parameters[1]}', 'rb') as f:
+                file_data = f.read(1024)
+                try:
+                    clientsocket.sendall((command + "/n").encode() + file_data)
+                except ConnectionResetError:
+                    # print("Error: Connection interrupted.")
+                    register_status(system_output, "Error: Connection interrupted.")
+                    return
+                
+                data = clientsocket.recv(1024)
+                if data == b'1':
+                    # print(f"Successfully stored {parameters[1]} to server_dir.")
+                    register_status(system_output, f"Successfully stored {parameters[1]} to server_dir.")
+                else:
+                    # print("Error: Failed to store file to server_dir.")
+                    register_status(system_output, "Error: Failed to store file to server_dir.")
+                    return
+        
+        if prefix_command == '/get':
+            if len(parameters) != 2:
+                # print("Error: Command parameters mismatch.")
+                register_status(system_output, "Error: Command parameters mismatch.")
+                return
+            
+            try:
+                clientsocket.sendall(command.encode())
+            except ConnectionResetError:
+                # print("Error: Connection interrupted.")
+                register_status(system_output, "Error: Connection interrupted.")
+                return
+            
+            header = clientsocket.recv(1024)
+
+            if header == b'0':
+                print("Error: File does not exist.")
+                return
+            elif header == b'1':
+                file_data = clientsocket.recv(1024)
+                with open(f'client_dir/{parameters[1]}', 'wb') as f:
+                    f.write(file_data)
+                    # print(f"Successfully retrieved {parameters[1]} from server_dir.")
+                    register_status(system_output, f"Successfully retrieved {parameters[1]} from server_dir.")
+            else:
+                # print("Error: Failed to retrieve file from server_dir.")
+                register_status(system_output, "Error: Failed to retrieve file from server_dir.")
+                return
+            
+        if prefix_command == '/leave':
+            if len(parameters) != 1:
+                print("Error: Command parameters mismatch.")
+                return
+            
+            clientsocket.sendall(command.encode())
+
+            data = clientsocket.recv(1024)
+            if data == b'1':
+                # print("Successfully left the server.")
+                register_status(system_output, "Successfully left the server.")
+                hasJoined = False
+            else:
+                # print("Error: Failed to leave the server.")
+                register_status(system_output, "Error: Failed to leave the server.")
+                return
+            
+            # create a new socket object
+            new_clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+            # unregister old socket object and register new socket
+            selector.unregister(clientsocket)
+            clientsocket.close()
+            clientsocket = new_clientsocket
+            selector.register(clientsocket, selectors.EVENT_READ)
+
+    else:
+        try:
+            clientsocket.sendall(command.encode())
+        except ConnectionResetError:
+            # print("Error: Connection interrupted.")
+            register_status(system_output, "Error: Connection interrupted.")
+            return
+        
+        data = clientsocket.recv(1024)
+        print(data.decode())
+
+    input_field.delete(0, tk.END)
+    
+def register_status(widget, status):
+    widget.config (state = tk.NORMAL)
+    widget.insert (tk.END, status)
+    widget.config (state = tk.DISABLED)
+            
+root = tk.Tk()
+
+# root.after(1000, message_check)
+
+label = tk.Label(root, text="Enter a command", fg="black", font=("Helvetica", 16))
+label.grid(row=0, column=0, columnspan=2)
+
+input_field = tk.Entry(root)
+input_field.grid(row=3, column=0, columnspan=2)
+
+send_button = tk.Button(root, text="Send", command=register_command)
+send_button.grid(row=4, column=0, columnspan=2)
+
+# system_output and unicast/broadcast_output
+
+system_output_label = tk.Label(root, text="System Output", fg="black", font=("Helvetica", 16))
+system_output_label.grid(row=5, column=0)
+
+system_output = scrolledtext.ScrolledText(root, width=40, height=25, state=tk.DISABLED)
+system_output.grid(row=6, column=0)
+
+unicast_output_label = tk.Label(root, text="Broadcast/Unicast Output", fg="black", font=("Helvetica", 16))
+unicast_output_label.grid(row=5, column=1)
+
+unicast_output = scrolledtext.ScrolledText(root, width=40, height=25, state=tk.DISABLED)
+unicast_output.grid(row=6, column=1)
+
+root.title("CSNETWK Machine Project - Client")
+root.geometry("630x475")
+
+selector.register(clientsocket, selectors.EVENT_READ)
+
+root.mainloop()
---
+++ server.py
@@ -0,0 +1,206 @@
+import socket, os, time, argparse
+from _thread import * 
+
+# create a socket object
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# socket,AF_INET = address family of IPv4
+# socket.SOCK_STREAM = TCP socket
+
+serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+# SOL_SOCKET = Socket option level
+# SO_REUSEADDR = Reuse socket address
+# 1 = True
+
+parser = argparse.ArgumentParser(description='Server script')
+parser.add_argument('--host', type=str, help='Host IP address', default='localhost')
+parser.add_argument('--port', type=int, help='Port number', default=3000)
+args = parser.parse_args()
+
+host = args.host
+port = args.port
+
+serversocket.bind((host, port))
+
+serversocket.settimeout(0.5)
+
+serversocket.listen(5)
+
+clients_connected = {}
+
+clients_registered = set()
+
+commands = {
+    '/leave': 1,        # sample: /leave
+    '/register': 2,     # sample: /register <username>
+    '/store': 2,        # sample: /store <filename>
+    '/dir': 1,          # sample: /dir
+    '/get': 2,          # sample: /get <filename>
+    '/?': 1,            # sample: /?
+    '/message': 30,     # sample: /message <client> <message>
+    '/broadcast': 30,   # sample: /broadcast <message>
+
+}
+
+def client_thread(conn):
+    print('Client connected')
+    
+    connected = True
+    client_name = None
+    clients_connected[conn] = client_name
+
+    print(clients_connected) # for debugging purposes
+
+    while connected: 
+        try:
+            command = conn.recv(1024).decode()
+        except ConnectionResetError:
+            break # close connection if client has disconnected
+
+        command_args = command.split('\n')[0].split()
+
+        print(repr(command), command_args) # for debugging purposes
+
+        # if command does not exist, send error message
+        if not (command_args[0] in commands):
+            conn.sendall('Invalid command'.encode())
+            continue
+
+        # if command exists and length of command_args != to required number of arguments, send error message
+        elif command_args[0] in commands and len(command_args) != commands[command_args[0]]:
+            # validation for /message and /broadcast, limit message to 30 characters
+            if command_args[0] in ['/message', '/broadcast'] and len(command_args) > 30:
+                conn.sendall('Error: 30 characters limit exceeded.'.encode())
+                continue
+            else:
+                conn.sendall('Error: Command parameters mismatch.'.encode())
+                continue
+        
+        # check if client has already registered a name
+        if not (client_name in clients_registered):
+            # if not, only allow /register, /leave, and /? commands
+            if command_args[0] not in ['/register', '/leave', '/?']:
+                conn.sendall('Error: Please register a name first.'.encode())
+                continue
+        else:
+            # if already registered and command is /register, send error message
+            if command_args[0] in ['/register']:
+                conn.sendall('Error: You have already registered a name.'.encode())
+                continue
+
+        match command_args[0]:
+            case '/leave':
+                conn.sendall(f"Connection closed.".encode())
+                broadcast_message(f"{client_name} has left the server.")
+
+                # remove client
+                clients_connected.pop(conn)
+                if client_name in clients_registered:
+                    clients_registered.remove(client_name)
+                conn = False
+
+            case '/register':
+                client_name = command_args[1] 
+                # username validation
+                if client_name is None:
+                    conn.sendall('Error: Please enter a valid username.'.encode())
+                elif client_name in clients_registered:
+                    conn.sendall('Error: Username already taken.'.encode())
+                else:
+                    clients_connected[conn] = client_name
+                    clients_registered.add(client_name)
+                    conn.sendall(f"Successfully registered as {client_name}.".encode())
+                    broadcast_message(f"{client_name} has joined the server.")
+
+            case '/dir': # list all files in server_dir
+                files = os.listdir('server_dir')
+                conn.sendall("Server Directory:\n".encode()+"\n".join(files).encode())
+
+            case '/store': # from client_dir to server_dir
+                file_name = command_args[1]
+                file_data = ('\n'.join(command.split('\n')[1:])).encode()
+                with open(f"server_dir/{file_name}", 'wb') as f:
+                    f.write(file_data)
+                    print(f"Successfully stored {file_name} in server_dir.")
+                
+                # get timestamp in format: 2023-11-06 16:48:05
+                time_stamp = os.path.getmtime(f"server_dir/{file_name}")
+                convert_time = time.localtime(time_stamp)
+                readable_time = time.strftime("%Y-%m-%d %H:%M:%S", convert_time)
+
+                conn.sendall(f"{client_name}<{readable_time}>: Uploaded {file_name}".encode())
+                broadcast_message(f"{client_name}<{readable_time}>: Uploaded {file_name}")
+
+            case '/get': # from server_dir to client_dir
+                file_name = command_args[1]
+                
+                files = os.listdir('server_dir')
+
+                if file_name not in files:
+                    conn.sendall(b'0') # send 0 if file does not exist
+                    conn.sendall(f"Error: {file_name} does not exist in the server.".encode())
+                    continue
+                
+                with open(f"server_dir/{file_name}", 'rb') as f:
+                    file_data = f.read()
+                    conn.sendall(b'1') # send 1 if file exists
+                    conn.sendall(file_data)
+                    print(f"Successfully retrieved {file_name} from server_dir.")
+
+            case '/?': # list all commands
+                message = """
+                /? - list all commands
+                /join <server_ip_> <port> - join the server
+                /register <username> - register a username
+                /dir - list all files in server_dir
+                /store <filename> - store file from client_dir to server_dir
+                /get <filename> - retrieve file from server_dir to client_dir
+                /message <client_username> <message> (30 characters limit) - send message to specific client
+                /broadcast <message> (30 characters limit) - send message to all clients
+                /leave - leave the server
+                """
+
+                conn.sendall(message.encode())
+
+            case '/message': # send message to specific client
+                to_client = command_args[1]
+                message = ' '.join(command_args[2:])
+
+                if to_client not in clients_registered:
+                    conn.sendall(f"Error: {to_client} is not registered.".encode())
+
+                for client_conn, check_to_client in clients_connected.items():
+                    if check_to_client == to_client:
+                        conn.sendall(f"To {to_client}: {message}".encode())
+                        client_conn.sendall(f"From {client_name}: {message}".encode())
+                    else:
+                        # if invalid to_client, send error message
+                        conn.sendall(f"Error: {to_client} is not connected.".encode())
+
+            case '/broadcast': # send message to all clients
+                message = ' '.join(command_args[1:])
+                broadcast_message(f"Broadcast from {client_name}: {message}")   
+
+            case _: # if command does not exist, send error message
+                conn.sendall('Error: Invalid command.'.encode())
+
+        # remove client when connection is closed
+        clients_connected.pop(conn)
+        if client_name in clients_registered:
+            clients_registered.remove(client_name)
+        conn.close()
+            
+
+def broadcast_message(message):
+    for client in clients_connected:
+        try:
+            client.sendall(message.encode())
+        except Exception as e:
+            print(f"Failed to send message to {client}: {e}")
+
+while True:
+    # establish connection with client
+    try:
+        conn, addr = serversocket.accept()
+        start_new_thread(client_thread, (conn,))
+    except TimeoutError:
+        pass
